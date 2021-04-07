89c226a1d34d557dee4001a27090cd1ef22cef43
Logan Ryan <loganryan982@gmail.com>
2021-04-07 11:54:55 +1000

    Added 3rd room

--- ParticleEffects/Assets/Scripts/SwitchScript.cs
+++ ParticleEffects/Assets/Scripts/SwitchScript.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class SwitchScript : MonoBehaviour
 {
     public Material switchMat;
-    
+    public GameObject gate;
+    private bool activate;
+
+    private void Update()
+    {
+        if (activate)
+        {
+            float dissolve = gate.GetComponent<MeshRenderer>().material.GetFloat("_DissolveAmount");
+
+            if (dissolve > 1)
+            {
+                activate = false;
+                Destroy(gate);
+            }
+
+            gate.GetComponent<MeshRenderer>().material.SetFloat("_DissolveAmount", dissolve + Time.deltaTime);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Projectile"))
         {
             gameObject.GetComponent<MeshRenderer>().material = switchMat;
+            activate = true;
         }
     }
 }
